127f435cff52eef7fd91a987a40a6b10c6944726
Algernon132 <40308777+Algernon132@users.noreply.github.com>
2019-12-14 16:16:07 -0600

    manipulate text

--- app/src/main/java/com/example/cdb9t2final/MainActivity.java
+++ app/src/main/java/com/example/cdb9t2final/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView text=findViewById(R.id.testDisplay);
+        text.setText("test");
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
